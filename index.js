9ceb9222ea6045195d5ed2336793bf7b6448ef3a
tanialx <taniathucle@gmail.com>
2021-05-18 11:28:49 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import * as core from '@actions/core'
+import { gh } from '@actions/github'
+
+run().catch(error => core.setFailed(error.message))
+
+async function run() {
+
+    const token = core.getInput('token')
+
+    const octokit = gh.getOctokit(token)
+
+    // paginate: receive all results across all pages
+    const user = gh.context.repo.owner
+    console.log(`Fetch repos owned by user ${user}`)
+    octokit.paginate("GET /users/{owner}/repos?type={type}", {
+        owner: user,
+        type: "owner"
+    }).then((repos) => {
+        console.log(`Found ${repos.length} repos`)
+        let repos_with_page = repos.filter(value => value.has_pages).map(value => {
+            return {
+                name: value.name,
+                has_pages: true,
+                description: value.description
+            }
+        })
+        core.setOutput('ghp', repos_with_page)
+    });
+}
